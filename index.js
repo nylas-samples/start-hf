47f7ad13ab1cc5c63f7e1acc6e5f47b8c99ffcfa
Ash Ryan Arnwine <sayhi@ashryan.io>
2024-07-09 21:39:50 -0400

    Fixing env var name.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import { HfInference } from "@huggingface/inference";
 
-const hfInference = new HfInference(process.env.HUGGINGFACE_API_KEY);
+const hfInference = new HfInference(process.env.HUGGING_FACE_API_KEY);
 
-const summarize = async function (prompt) {
+const promptLlm = async function (prompt) {
   const response = await hfInference.textGeneration({
     model: "mistralai/Mistral-7B-Instruct-v0.2",
     inputs: prompt,
@@ -14,6 +14,6 @@ const summarize = async function (prompt) {
 
 const prompt = "What's the best neighborhood in NYC?";
 
-const llmResponse = await summarize(prompt);
+const llmResponse = await promptLlm(prompt);
 
 console.log(llmResponse);
